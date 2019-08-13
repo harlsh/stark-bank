e31654b38dfd1337bf625590c234486a76494ad9
harish0796 <harish0796@gmail.com>
2019-08-13 16:29:51 +0530

    skeleton:

---
+++ stark-bank/src/main/java/com/dbs/starkbank/StarkBankApplication.java
@@ -0,0 +1,13 @@
+package com.dbs.starkbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarkBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarkBankApplication.class, args);
+    }
+
+}
