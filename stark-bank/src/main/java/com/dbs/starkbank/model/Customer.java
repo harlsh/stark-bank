47aa8ab4838f4c008e9f364bcc962978e617ad0c
harish0796 <harish0796@gmail.com>
2019-08-13 17:08:41 +0530

    adding skeleton

---
+++ stark-bank/src/main/java/com/dbs/starkbank/model/Customer.java
@@ -0,0 +1,16 @@
+package com.dbs.starkbank.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String phoneNumber;
+
+}
